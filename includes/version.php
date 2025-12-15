1e081d1f509912ed88f4a8d98d1211e339c03e54
websitetalkingheadsvideo <websitetalkingheadsvideo@gmail.com>
2025-12-14 22:40:41 -0700

    chore: organize canon clan files into subfolders and fix JSON validation

--- includes/version.php
+++ includes/version.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Centralized Version Management
- * Valley by Night - Version 0.8.32
+ * Valley by Night - Version 0.8.33
  */
 if (!defined('LOTN_VERSION')) {
-   define('LOTN_VERSION', '0.8.32');
+   define('LOTN_VERSION', '0.8.33');
 }
 
