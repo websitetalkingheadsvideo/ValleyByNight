7ea1455ecc3ea24cc57c232a4f3b8ae6710f28b5
websitetalkingheadsvideo <websitetalkingheadsvideo@gmail.com>
2025-12-26 14:29:20 -0700

    chore: standardize ghoul clan fields and fix NPC player names

--- includes/version.php
+++ includes/version.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Centralized Version Management
- * Valley by Night - Version 0.8.92
+ * Valley by Night - Version 0.8.93
  */
 if (!defined('LOTN_VERSION')) {
-   define('LOTN_VERSION', '0.8.92');
+   define('LOTN_VERSION', '0.8.93');
 }
 
