8546938ded168bfee39acdde1a1a3ae018c79a32
websitetalkingheadsvideo <websitetalkingheadsvideo@gmail.com>
2026-01-10 19:54:26 -0700

    chore: normalize paragraph structure in LotNR.md and bump version

--- includes/version.php
+++ includes/version.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Centralized Version Management
- * Valley by Night - Version 0.9.38
+ * Valley by Night - Version 0.9.39
  */
 if (!defined('LOTN_VERSION')) {
-   define('LOTN_VERSION', '0.9.38');
+   define('LOTN_VERSION', '0.9.39');
 }
 
