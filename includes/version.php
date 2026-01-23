30b9e192204866f79e116cbc7915d668d212ebf8
websitetalkingheadsvideo <websitetalkingheadsvideo@gmail.com>
2026-01-23 04:39:07 -0700

    chore: bump version to 0.9.56 and sync project cleanup

--- includes/version.php
+++ includes/version.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Centralized Version Management
- * Valley by Night - Version 0.9.55
+ * Valley by Night - Version 0.9.56
  */
 if (!defined('LOTN_VERSION')) {
-   define('LOTN_VERSION', '0.9.55');
+   define('LOTN_VERSION', '0.9.56');
 }
 
